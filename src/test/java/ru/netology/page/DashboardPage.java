e5f2d2068d83526edec0d34f9845e8acae3bdfe8
Kostya <tsarevkostya03@gmail.com>
2026-08-04 22:36:03 +0500

    Fix CI config, dashboard page and transfer page

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,32 +1,29 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.Keys;
+import ru.netology.data.DataHelper;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-    private final SelenideElement heading = $("[data-test-id='dashboard']");
+    private final ElementsCollection cards = $$("[data-test-id='card']");
 
     public int getCardBalance(String cardNumber) {
-        String cardNumberMasked = maskCardNumber(cardNumber);
-        String balanceText = $("[data-test-id='" + cardNumberMasked + "'] .balance")
-                .getText()
+        SelenideElement card = findCardByNumber(cardNumber);
+        String balanceText = card.$(".balance").getText()
                 .replaceAll("[^\\d]", "");
         return Integer.parseInt(balanceText);
     }
 
     public TransferPage selectCardForReplenish(String cardNumber) {
-        String cardNumberMasked = maskCardNumber(cardNumber);
-        SelenideElement cardElement = $("[data-test-id='" + cardNumberMasked + "']");
-        cardElement.$("[data-test-id='action-deposit']").click();
+        SelenideElement card = findCardByNumber(cardNumber);
+        card.$("[data-test-id='action-deposit']").click();
         return new TransferPage();
     }
 
-    private String maskCardNumber(String cardNumber) {
-        // Маскирование номера карты для data-test-id
-        String digits = cardNumber.replaceAll("\\s", "");
-        return "card-" + digits.substring(0, 4) + "-" + digits.substring(4, 8) + "-" + digits.substring(8, 12) + "-" + digits.substring(12);
+    private SelenideElement findCardByNumber(String cardNumber) {
+        // Ищем карту по тексту номера
+        return cards.findBy(text -> text.getText().contains(cardNumber));
     }
 }
--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -9,16 +9,22 @@ public class TransferPage {
     private final SelenideElement fromCardField = $("[data-test-id='from'] input");
     private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
 
+    // Основной метод для перевода
     public DashboardPage makeTransfer(int amount, String fromCardNumber) {
-        amountField.setValue(String.valueOf(amount));
-        fromCardField.setValue(fromCardNumber);
+        fillTransferForm(amount, fromCardNumber);
         transferButton.click();
         return new DashboardPage();
     }
 
+    // Метод для некорректных переводов (без возврата на Dashboard)
     public void makeInvalidTransfer(int amount, String fromCardNumber) {
+        fillTransferForm(amount, fromCardNumber);
+        transferButton.click();
+    }
+
+    // Вынесенный общий метод для заполнения формы
+    private void fillTransferForm(int amount, String fromCardNumber) {
         amountField.setValue(String.valueOf(amount));
         fromCardField.setValue(fromCardNumber);
-        transferButton.click();
     }
 }
